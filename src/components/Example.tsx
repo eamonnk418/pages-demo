53d60af6dfb6533fe95731173538374f82052738
eamonnk418 <160419984+eamonnk418@users.noreply.github.com>
2024-06-12 19:58:11 +0100

    Example component

---
+++ src/components/Example.tsx
@@ -0,0 +1,10 @@
+export const Example = () => {
+    return (
+        <main>
+            <h1>Title</h1>
+            <article>
+                <aside>Some content to the aside</aside>
+            </article>
+        </main>
+    );
+}
